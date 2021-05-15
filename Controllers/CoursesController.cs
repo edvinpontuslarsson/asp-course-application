7f3dcf18e36cf94e035dd4de31b0ebaf3e51ca77
Edvin Larsson <el223na@student.lnu.se>
2021-05-15 21:21:13 +0200

    Get courses (#2)
    * adds courses
    
    * adds controller
    
    * courses controller returns empty obj
    
    * returns json file content

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        // GET: api/Courses
+        [HttpGet]
+        public string GetCourses()
+        {
+            return System.IO.File.ReadAllText(@"courses.json");
+        }
+    }
+}
